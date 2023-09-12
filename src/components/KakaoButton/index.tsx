36451de55757b0174953efe6938799fc651c81d9
anonymousrecords <a.Records2022@gmail.com>
2023-09-12 11:22:32 +0900

    🎀 [CHORE] : app router -> page router 변경

---
+++ src/components/KakaoButton/index.tsx
@@ -0,0 +1,16 @@
+// import Image from 'next/image';
+// import Link from 'next/link';
+
+// interface KakaoButtonProps {
+//   className?: string;
+// }
+
+// export default function KakaoButton(KakaoButtonProps) {
+//   return (
+//     <Link
+//       href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_KAKAO_REDIRECT_URI}&response_type=code`}
+//     >
+//       <Image src="/assets/kakao_login_button.png" alt="kakao-login" width={258.048} height={56} />
+//     </Link>
+//   );
+// }
