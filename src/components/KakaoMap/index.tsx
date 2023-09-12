6c6247b39ee520897e0fac69b1ff49b4d7738a33
anonymousrecords <a.Records2022@gmail.com>
2023-09-13 02:57:26 +0900

    ✨ [FEAT] : 기본 마커 생성

--- src/components/KakaoMap/index.tsx
+++ src/components/KakaoMap/index.tsx
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
 
@@ -8,9 +8,12 @@ const KakaoMap = () => {
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
       <Map
-        center={{ lat: 33.450701, lng: 126.570667 }}
+        center={{ lat: 37.55130586957679, lng: 126.98821364692621 }}
         style={{ width: '100%', height: '100%' }}
-      ></Map>
+        level={5}
+      >
+        <MapMarker position={{ lat: 37.55130586957679, lng: 126.98821364692621 }}></MapMarker>
+      </Map>
     </>
   );
 };
