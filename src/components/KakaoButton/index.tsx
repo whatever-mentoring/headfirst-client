749b8b177376c95271c868c997ba383f1e5a6fbd
anonymousrecords <a.Records2022@gmail.com>
2023-09-12 14:55:44 +0900

    ✨ [FEAT] : 카카오로그인 일부 구현

--- src/components/KakaoButton/index.tsx
+++ src/components/KakaoButton/index.tsx
@@ -1,16 +1,23 @@
-// import Image from 'next/image';
-// import Link from 'next/link';
+import Image from 'next/image';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-// interface KakaoButtonProps {
-//   className?: string;
-// }
-
-// export default function KakaoButton(KakaoButtonProps) {
-//   return (
-//     <Link
-//       href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_KAKAO_REDIRECT_URI}&response_type=code`}
-//     >
-//       <Image src="/assets/kakao_login_button.png" alt="kakao-login" width={258.048} height={56} />
-//     </Link>
-//   );
-// }
+export default function KakaoButton() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        {session.user?.name}님 반갑습니다. <br />
+        <button onClick={() => signOut()}>로그아웃</button>
+      </>
+    );
+  }
+  return (
+    <Image
+      onClick={() => signIn('kakao')}
+      src="/assets/kakao_login_button.png"
+      alt="kakao-login"
+      width={258.048}
+      height={56}
+    />
+  );
+}
