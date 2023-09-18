0bbe3a51e0a76cdaf55091e3226c08bd9ba2c8bf
anonymousrecords <a.Records2022@gmail.com>
2023-09-18 15:07:59 +0900

    ✨ [FEAT] : 카카오로그인 임시 구현

--- src/components/KakaoButton/index.tsx
+++ src/components/KakaoButton/index.tsx
@@ -1,23 +1,50 @@
+import * as React from 'react';
+import Router from 'next/router';
 import Image from 'next/image';
-import { signIn, signOut, useSession } from 'next-auth/react';
 
-export default function KakaoButton() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        {session.user?.name}님 반갑습니다. <br />
-        <button onClick={() => signOut()}>로그아웃</button>
-      </>
-    );
+const kakaoInit = () => {
+  const kakao = (window as any).Kakao;
+  if (!kakao.isInitialized()) {
+    kakao.init(process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY);
   }
+
+  return kakao;
+};
+
+const Index = () => {
+  const kakaoLogin = async () => {
+    // 카카오 초기화
+    const kakao = kakaoInit();
+
+    // 카카오 로그인 구현
+    kakao.Auth.login({
+      success: () => {
+        kakao.API.request({
+          url: '/v2/user/me', // 사용자 정보 가져오기
+          success: (res: any) => {
+            console.log(res);
+            Router.push('/main');
+          },
+          fail: (error: any) => {
+            console.log(error);
+          },
+        });
+      },
+      fail: (error: any) => {
+        console.log(error);
+      },
+    });
+  };
+
   return (
     <Image
-      onClick={() => signIn('kakao')}
+      onClick={kakaoLogin}
       src="/assets/kakao_login_button.png"
       alt="kakao-login"
       width={258.048}
       height={56}
     />
   );
-}
+};
+
+export default Index;
