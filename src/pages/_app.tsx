b764198c623c5df7eee3e59a445c6321cd25bace
anonymousrecords <a.Records2022@gmail.com>
2023-09-18 12:41:35 +0900

    ✨ [FEAT] : next-auth를 이용한 카카오로그인

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,10 @@ import '@/styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <RecoilRoot>
-      <SessionProvider>
+      <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
     </RecoilRoot>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type AuthOptions } from 'next-auth';
 import KakaoProvider from 'next-auth/providers/kakao';
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     KakaoProvider({
       clientId: process.env.NEXT_KAKAO_CLIENT_ID!,
       clientSecret: process.env.NEXT_KAKAO_CLIENT_SECRET!,
     }),
   ],
-});
+};
+
+export default NextAuth(authOptions);
