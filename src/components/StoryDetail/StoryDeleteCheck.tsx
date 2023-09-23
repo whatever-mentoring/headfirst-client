e560e694d00bdc97024e69647ff5257a1696520d
박지윤 <tkrhkfld123@naver.com>
2023-09-23 16:45:06 +0900

    ➕ [ADD] 부수적인 코드 작성

--- src/components/StoryDetail/StoryDeleteCheck.tsx
+++ src/components/StoryDetail/StoryDeleteCheck.tsx
@@ -3,13 +3,13 @@ import type { NextPage } from 'next';
 
 const StoryDeleteCheck: NextPage = () => {
   return (
-    <>
-      <div className="cursor-pointer fixed w-[361px] h-[70px] bg-[#FCFCFC] mt-[757px]">
+    <div className="bg-pink-100 fixed">
+      <div className="cursor-pointer fixed w-[361px] h-[70px] bg-[#FCFCFC] mt-[350px] ml-[-59.7px] ">
         <div>
-          <text className="text-[#000000] ml-[143px] mt-[21px]"> 삭제하기 </text>
+          <text className="text-[#000000] ml-[143px] pt-[21px] pt-[21px] font-light">삭제하기</text>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
