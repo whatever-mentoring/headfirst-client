c5046f63b00f2a18e8990b21615e154069e8b980
박지윤 <tkrhkfld123@naver.com>
2023-09-24 16:47:37 +0900

    ✨ [FEAT] 사연 작성 페이지 POST 요청 완료

--- src/components/CreateStory/CreateStoryButton.tsx
+++ src/components/CreateStory/CreateStoryButton.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next';
 import CreateStoryModal from './createStoryModal';
 import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
-import { createStoryModalState, timeState } from '@/states/createStoryState';
-import { inputValueState, textareaValueState } from '@/states/createStoryState';
+import {
+  ResInputState,
+  ResTextareaState,
+  createStoryModalState,
+  timeState,
+  ResMemberId,
+  inputValueState,
+  textareaValueState,
+} from '@/states/createStoryState';
 import axios from 'axios';
+import { getCookie } from 'cookies-next';
 // import { keywordState } from '@/states/createStoryState';
 
 const CreateStoryButton: NextPage = () => {
@@ -11,6 +19,9 @@ const CreateStoryButton: NextPage = () => {
   const inputValue = useRecoilValue(inputValueState);
   const textareaValue = useRecoilValue(textareaValueState);
   const setTime = useSetRecoilState(timeState);
+  const setResInput = useSetRecoilState(ResInputState);
+  const setResTextarea = useSetRecoilState(ResTextareaState);
+  const setResMemberId = useSetRecoilState(ResMemberId);
   // const [keyword, setKeyword] = useRecoilState(keywordState);
 
   const modalHandle = async () => {
@@ -37,14 +48,17 @@ const CreateStoryButton: NextPage = () => {
         'http://api.headfirst.p-e.kr/api/story/register',
         requestData,
         {
-          // headers: {
-          //   Authorization: `Bearer ${cookieToken}`,
-          // },
+          headers: {
+            Authorization: `Bearer ${getCookie('accessToken')}`,
+          },
         },
       );
 
       // 서버로부터의 응답을 처리합니다.
       console.log('서버 응답:', response.data);
+      setResInput(response.data.data.title);
+      setResTextarea(response.data.data.content);
+      setResMemberId(response.data.data.member_id);
     } catch (error) {
       // 에러를 처리합니다.
       console.error('에러:', error);
