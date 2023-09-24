3826c816eee58556d1cf50fd4d9423e9be03fa59
anonymousrecords <a.Records2022@gmail.com>
2023-09-25 08:42:14 +0900

    ⚰️ [DEL] :  카카오맵 관련 임시 코드 삭제

--- src/components/KakaoMap/index.tsx
+++ src/components/KakaoMap/index.tsx
@@ -1,132 +1,138 @@
-// import { Map, MapMarker, CustomOverlayMap } from 'react-kakao-maps-sdk';
-// import React, { useState, useEffect, useRef } from 'react';
-// import { useRecoilValue } from 'recoil';
-// import { SearchXAtom } from '@/recoil/SearchXAtom';
-// import { SearchYAtom } from '@/recoil/SearchYAtom';
+/** 지도 위치 연습
+ * 1. 임시 위치 불러오기 : 성공
+ * 2. (현재 위치 파악 시)현재 위치 불러오기 : 성공
+ * 3. 검색한 주소 위치 불러오기 : 실패
+ * 4. (현재 위치 클릭 시)현재 위치 불러오기
+ * */
 
-// interface KakaoMapProps {
-//   openNewStoryModal: () => void;
-//   openAddStoryModal: () => void;
-// }
+import { Map, MapMarker, CustomOverlayMap } from 'react-kakao-maps-sdk';
+import { useRecoilValue } from 'recoil';
+import { SearchXAtom } from '@/recoil/SearchXAtom';
+import { SearchYAtom } from '@/recoil/SearchYAtom';
+import { useState, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { NewStoryMarkerAtom } from '@/recoil/NewStoryMarkerAtom';
+import { AddStoryMarkerAtom } from '@/recoil/AddStoryMarkerAtom';
+import { AddStoryMarkerTitle } from '@/recoil/AddStoryMarkerTitle';
 
-// const KakaoMap: React.FC<KakaoMapProps> = ({ openNewStoryModal, openAddStoryModal }) => {
-//   // const [map, setMap] = useState<null | kakao.maps.Map>(null);
+interface KakaoMapProps {
+  openNewStoryModal: () => void;
+  openAddStoryModal: () => void;
+}
 
-//   // 임시 위치 데이터
-//   // const temporaryLocation = {
-//   //   title: '가천관',
-//   //   latlng: { lat: 37.45014238433003, lng: 127.12972934051282 },
-//   // };
+export default function KakaoMap({ openNewStoryModal, openAddStoryModal }: KakaoMapProps) {
+  // 임시 마커 데이터
+  const locations = [
+    { title: '가천관', latlng: { lat: 37.45014238433003, lng: 127.12972934051282 } },
+    { title: '올데이즈 커피', latlng: { lat: 37.44756362788988, lng: 127.12729516027275 } },
+    { title: 'AI 공학과', latlng: { lat: 37.4551613, lng: 127.1334068 } },
+  ];
 
-//   // 임시 마커 데이터
-//   const locations = [
-//     { title: '가천관', latlng: { lat: 37.45014238433003, lng: 127.12972934051282 } },
-//     { title: '올데이즈 커피', latlng: { lat: 37.44756362788988, lng: 127.12729516027275 } },
-//     { title: 'AI 공학과', latlng: { lat: 37.4551613, lng: 127.1334068 } },
-//   ];
+  // 1. 임시 위치 불러오기 : 임시 위치 데이터
+  const temporaryLocation = {
+    title: '가천관',
+    latlng: { lat: 37.45014238433003, lng: 127.12972934051282 },
+  };
 
-//   useEffect(() => {
-//     navigator.geolocation.getCurrentPosition(successHandler, errorHandler); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
-//   }, []);
+  // 2. (현재 위치 파악 시)현재 위치 불러오기 : 성공
+  const [userLocation, setUserLocation] = useState<{ lat: number; lng: number } | null>(null);
 
-//   // 위치 검색
-//   const placeX = useRecoilValue(SearchXAtom);
-//   const placeY = useRecoilValue(SearchYAtom);
-//   console.log('검색한 위치', placeX, placeY);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+        },
+      );
+    } else {
+      console.error('Geolocation is not available.');
+    }
+  }, []);
 
-//   // geolocation 현재 위치
-//   const [location, setLocation] = useState<
-//     | GeolocationCoordinates
-//     | { lat: number; lng: number }
-//     | { latitue: number; longtitude: number }
-//     | null
-//   >(null); // 현재 위치를 저장할 상태
+  console.log('유저 위치의 현재 위치 : ', userLocation);
 
-//   const successHandler = (response: GeolocationPosition) => {
-//     console.log('현재 위치', response);
-//     // console.log('위도', response.coords.latitude);
-//     const { coords } = response; // coords 객체에서 추출
+  // 위치 검색
+  const placeX = useRecoilValue<number>(SearchXAtom);
+  const placeY = useRecoilValue<number>(SearchYAtom);
 
-//     // placeX와 placeY가 null이 아닌 경우에만 해당 위치로 이동
-//     if (placeX !== null && placeY !== null) {
-//       setLocation({ lat: placeX[0], lng: placeY[0] });
-//     } else {
-//       setLocation({ lat: coords.latitude, lng: coords.longitude }); // GeolocationCoordinates를 설정
-//     }
+  // placeX와 placeY를 사용하여 latlng 객체 생성
+  const searchLocation = placeX && placeY ? { lat: Number(placeX), lng: Number(placeY) } : null;
+  console.log('검색한 위치', searchLocation);
 
-//     console.log('coords에 대해 알아보자 : ', coords.latitude);
-//     setLocation({ lat: coords.latitude.getLat(), lng: coords.longitude.getLng() });
-//     setLocation(coords);
+  // 임시 위치, 현재 위치, 또는 검색 위치를 사용하여 중심 좌표 설정
+  //   const center = searchLocation || userLocation || temporaryLocation.latlng;
+  //   const center = userLocation || temporaryLocation.latlng;
+  const center = temporaryLocation.latlng;
 
-//     console.log('location이 뭐 찍힐까? : ', location);
-//   };
+  // NewStory 마커
+  const [markerPosition, setMarkerPosition] = useState(center);
+  const [newStoryMarkerPosition, setNewStoryMarkerPosition] = useRecoilState(NewStoryMarkerAtom);
+  const onMarkerDragEnd = (marker: any) => {
+    // 마커의 새로운 위치를 상태 변수에 업데이트
+    const newPosition = {
+      lat: marker.getPosition().getLat(),
+      lng: marker.getPosition().getLng(),
+    };
+    setMarkerPosition(newPosition);
+    console.log(markerPosition);
+    setNewStoryMarkerPosition((prevPositions) => [...prevPositions, newPosition]);
+    console.log('NewStory 마커 위치 :', newStoryMarkerPosition);
+  };
 
-//   const errorHandler = (error: GeolocationPositionError) => {
-//     console.log(error);
-//   };
+  // AddStory 마커
+  const [addStoryMarkerTitle, setAddStoryMarkerTitle] = useRecoilState(AddStoryMarkerTitle);
+  const [addStoryMarkerPosition, setAddStoryMarkerPosition] = useRecoilState(AddStoryMarkerAtom);
+  const onAddStoryMarkerClick = (loc: any) => {
+    setAddStoryMarkerPosition(loc);
+    setAddStoryMarkerTitle(loc.title);
+    console.log('addStory 마커 제목', addStoryMarkerTitle);
+    console.log('addStory 마커', addStoryMarkerPosition);
+  };
 
-//   // 현재 위치 좌표값
-//   const [coordinates, setCoordinates] = useState<{ lat: number; lng: number } | null>(null);
-//   const mapRef = useRef<Map | null>(null);
-
-//   const getCoordinates = () => {
-//     if (mapRef.current) {
-//       const map = mapRef.current;
-
-//       setCoordinates({
-//         lat: map.getCenter().getLat(),
-//         lng: map.getCenter().getLng(),
-//       });
-//     }
-//   };
-//   console.log('위도&경도 : ', coordinates);
-
-//   return (
-//     <>
-//       {location && (
-//         <Map
-//           center={{
-//             lat: coordinates?.lat,
-//             lng: coordinates?.lng,
-//           }}
-//           style={{ width: '100%', height: '100%' }}
-//           level={4}
-//           ref={mapRef}
-//           onClick={getCoordinates}
-//         >
-//           <CustomOverlayMap
-//             position={{
-//               lat: location.latitude,
-//               lng: location.longitude,
-//             }}
-//           >
-//             {locations.map((loc) => (
-//               <MapMarker
-//                 key={`${loc.title}-${loc.latlng.lat}-${loc.latlng.lng}`}
-//                 position={loc.latlng}
-//                 image={{
-//                   src: '/assets/marker.svg',
-//                   size: { width: 50, height: 50 },
-//                 }}
-//                 title={loc.title}
-//                 onClick={openAddStoryModal}
-//               />
-//             ))}
-//             {/* 현재 위치 좌표 */}
-//             <MapMarker
-//               position={{ lat: location.latitude, lng: location.longitude }}
-//               image={{
-//                 src: '/assets/current-marker.svg',
-//                 size: { width: 36, height: 44 },
-//               }}
-//               onClick={openNewStoryModal}
-//               draggable={true}
-//             />
-//           </CustomOverlayMap>
-//         </Map>
-//       )}
-//     </>
-//   );
-// };
-
-// export default KakaoMap;
+  return (
+    <Map // 지도를 표시할 Container
+      id="map"
+      center={center}
+      style={{
+        // 지도의 크기
+        width: '100%',
+        height: '100%',
+      }}
+      level={3} // 지도의 확대 레벨
+    >
+      <CustomOverlayMap position={center}>
+        {/* AddStory 마커 */}
+        {locations.map((loc) => (
+          <MapMarker
+            key={`${loc.title}-${loc.latlng.lat}-${loc.latlng.lng}`}
+            position={loc.latlng}
+            image={{
+              src: '/assets/marker.svg',
+              size: { width: 50, height: 50 },
+            }}
+            title={loc.title}
+            onClick={() => {
+              onAddStoryMarkerClick(loc);
+              openAddStoryModal();
+            }}
+          />
+        ))}
+        {/* NewStory 마커 */}
+        <MapMarker
+          position={markerPosition}
+          image={{
+            src: '/assets/current-marker.svg',
+            size: { width: 36, height: 44 },
+          }}
+          onClick={openNewStoryModal}
+          draggable={true}
+          onDragEnd={onMarkerDragEnd}
+        />
+      </CustomOverlayMap>
+    </Map>
+  );
+}
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,74 +1,44 @@
-// import type { NextPage } from 'next';
-// import KakaoMap from '@/components/KakaoMap';
-// import MainSearchBar from '@/components/MainSearchBar';
-// import ProfileButton from '@/components/ProfileButton';
-// import AddStoryModal from '@/components/AddStoryModal';
-// import { useState } from 'react';
-// import NewStoryModal from '@/components/NewStoryModal';
-// import SignInModal from '@/components/SignInModal';
+import KakaoMap from '@/components/KakaoMap';
+import AddStoryModal from '@/components/AddStoryModal';
+import { useState } from 'react';
+import NewStoryModal from '@/components/NewStoryModal';
+import SignInModal from '@/components/SignInModal';
 
-// const Main: NextPage = () => {
-//   // AddStoryModal
-//   const [isAddStoryModalVisible, setIsAddStoryModalVisible] = useState(false);
+type ModalTypes = 'addStory' | 'newStory' | 'signIn';
 
-//   const openAddStoryModal = () => {
-//     setIsAddStoryModalVisible(true);
-//   };
+const Main = () => {
+  // 모달창 관리
+  const [activeModal, setActiveModal] = useState(null);
 
-//   const closeAddStoryModal = () => {
-//     setIsAddStoryModalVisible(false);
-//   };
+  const openModal = (modalType: ModalTypes) => {
+    setActiveModal(modalType);
+  };
 
-//   // NewStoryModal
-//   const [isNewStoryModalVisible, setIsNewStoryModalVisible] = useState(false);
+  const closeModal = () => {
+    setActiveModal(null);
+  };
 
-//   const openNewStoryModal = () => {
-//     setIsNewStoryModalVisible(true);
-//   };
+  return (
+    <div className="flex h-screen items-center justify-center bg-red-200">
+      <div className="flex w-[360px] h-screen mx-auto bg-white flex-col">
+        <KakaoMap
+          openNewStoryModal={() => openModal('newStory')}
+          openAddStoryModal={() => openModal('addStory')}
+        />
+        <div className="flex items-center justify-center">
+          {activeModal === 'addStory' && <AddStoryModal onCloseClick={closeModal} />}
+          {activeModal === 'newStory' && (
+            <NewStoryModal
+              title="NewStoryModal"
+              subtitle="NewStoryModal입니다"
+              onCloseClick={closeModal}
+            />
+          )}
+          {activeModal === 'signIn' && <SignInModal onCloseClick={closeModal} />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-//   const closeNewStoryModal = () => {
-//     setIsNewStoryModalVisible(false);
-//   };
-
-//   // SignInModal
-//   const [isSignInModalVisible, setIsSignInModalVisible] = useState(false);
-
-//   // const openAddStoryModal = () => {
-//   //   setIsAddStoryModalVisible(true);
-//   // };
-
-//   const closeSignInModal = () => {
-//     setIsSignInModalVisible(false);
-//   };
-
-//   return (
-//     <div className="flex h-screen items-center justify-center bg-red-200">
-//       <main className="w-[360px] h-screen absolute">
-//         <header className="fixed top-[29.61px] z-10 flex items-center justify-center gap-[8.51px]">
-//           <MainSearchBar />
-//           <ProfileButton width={50} height={50} />
-//         </header>
-//         <KakaoMap openNewStoryModal={openNewStoryModal} openAddStoryModal={openAddStoryModal} />
-//         <div className="flex items-center justify-center">
-//           {isAddStoryModalVisible && (
-//             <AddStoryModal
-//               // title="AddStoryModal"
-//               // subtitle="AddStoryModal입니다"
-//               onCloseClick={closeAddStoryModal}
-//             />
-//           )}
-//           {isNewStoryModalVisible && (
-//             <NewStoryModal
-//               title="NewStoryModal"
-//               subtitle="NewStoryModal입니다"
-//               onCloseClick={closeNewStoryModal}
-//             />
-//           )}
-//           {isSignInModalVisible && <SignInModal onCloseClick={closeSignInModal} />}
-//         </div>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Main;
+export default Main;
