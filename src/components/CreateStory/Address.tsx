6b5bc0578fe221bca7ba4c7a98ffb60db83459e5
anonymousrecords <a.Records2022@gmail.com>
2023-09-25 18:41:23 +0900

    ✅ [MOD] : Address 하드코드 삭제

--- src/components/CreateStory/Address.tsx
+++ src/components/CreateStory/Address.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
-
 import Icon from '@/../public/assets/Icon.svg';
+import { useRecoilValue } from 'recoil';
+import { NewStoryMarkerTitle } from '@/recoil/NewStoryMarkerTitle';
+import { NewStoryMarkerSub } from '@/recoil/NewStoryMarkerSub';
+import { AddStoryMarkerTitle } from '@/recoil/AddStoryMarkerTitle';
+import { AddStoryMarkerSub } from '@/recoil/AddStoryMarkerSub';
 
 const Address: NextPage = () => {
+  const title = useRecoilValue(NewStoryMarkerTitle || AddStoryMarkerTitle);
+  const sub = useRecoilValue(NewStoryMarkerSub || AddStoryMarkerSub);
   return (
     <div className="flex">
       <Image
@@ -13,12 +19,10 @@ const Address: NextPage = () => {
       ></Image>
       <div className="flex-inline">
         <div className="flex">
-          <div className="mt-[17px]">서초타운트라팰리스아파트</div>
+          <div className="mt-[17px]">{title}</div>
           <p className="mt-[23px] ml-[4px] text-xs text-createStorySubAddress">아파트</p>
         </div>
-        <div className="text-sm text-createStorySubAddress">
-          서울 서초구 서초대로 74길 23 (서초동)
-        </div>
+        <div className="text-sm text-createStorySubAddress">{sub}</div>
       </div>
     </div>
   );
